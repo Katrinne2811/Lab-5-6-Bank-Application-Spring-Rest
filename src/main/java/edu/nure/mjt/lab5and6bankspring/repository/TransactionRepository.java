acf1679cdb2bed923c8359c1ccfd66e3fa4497d0
Katrinne2811 <160325742+Katrinne2811@users.noreply.github.com>
2025-05-28 13:27:20 -0600

    Update TransactionRepository.java

--- src/main/java/edu/nure/mjt/lab5and6bankspring/repository/TransactionRepository.java
+++ src/main/java/edu/nure/mjt/lab5and6bankspring/repository/TransactionRepository.java
@@ -1,8 +1,1 @@
-package edu.nure.mjt.lab5and6bankspring.repository;
 
-import edu.nure.mjt.lab5and6bankspring.model.entity.Transaction;
-import org.springframework.data.repository.CrudRepository;
-
-//Інтерфейс для збереження даних про транзакції з їх id з можливістю виконання CRUD-операцій над цими даними
-public interface TransactionRepository extends CrudRepository<Transaction, Long> {
-}
